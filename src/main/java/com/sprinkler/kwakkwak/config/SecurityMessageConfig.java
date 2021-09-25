fa3f638f8a53f597705099685c0b3c052c4bbce3
gi-ho Lee <52477052+kc2585@users.noreply.github.com>
2021-09-25 16:53:49 +0900

    Add files via upload
    로그인 & 회원가입 init

---
+++ src/main/java/com/sprinkler/kwakkwak/config/SecurityMessageConfig.java
@@ -0,0 +1,9 @@
+package com.sprinkler.kwakkwak.config;
+
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+public class SecurityMessageConfig {
+}
