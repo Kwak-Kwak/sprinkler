91f33899a7c39efb2b5657cce436951060ce533e
kc2585 <52477052+kc2585@users.noreply.github.com>
2021-09-25 14:41:12 +0900

    Login & Sing up init

---
+++ src/main/java/com/sprinkler/kwakkwak/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.sprinkler.kwakkwak.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!");
+        return "main";
+    }
+}
