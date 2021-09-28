d8b1c7cf31d292205634ba0fc406f17ee11fb9c2
gi-ho Lee <52477052+kc2585@users.noreply.github.com>
2021-09-28 22:07:27 +0900

    Add files via upload

---
+++ src/main/java/com/sprinkler/kwakkwak/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.sprinkler.kwakkwak.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("main");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/singup").setViewName("singup");
+    }
+}
