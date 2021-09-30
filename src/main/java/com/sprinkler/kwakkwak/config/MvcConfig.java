196fadc6d7ae50aef97e144fc0ef2a2b535b34f8
gi-ho Lee <52477052+kc2585@users.noreply.github.com>
2021-09-30 22:47:32 +0900

    Update MvcConfig.java

--- src/main/java/com/sprinkler/kwakkwak/config/MvcConfig.java
+++ src/main/java/com/sprinkler/kwakkwak/config/MvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("main");
+        registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/admin").setViewName("admin");
         registry.addViewController("/singup").setViewName("singup");
